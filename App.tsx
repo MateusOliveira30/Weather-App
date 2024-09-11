5071192d6d9449174b51964d7931bdb422670b61
MateusOliveira30 <mcesaroliveira30@yahoo.com>
2024-09-11 12:16:40 -0300

    feat: putting images and set rain, wind and humidity area

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
@@ -9,6 +9,37 @@ export default function App() {
         <Text>Stockholm, Sweden</Text>
         <Text>Tue, Jun 30</Text>
         <StatusBar style="auto" />
+      </View>
+      <View style={styles.container}>
+        <Image source={require('./assets/cloudy.png')} style={{ width: 100, height: 100 }} />
+        <Text>19 °C</Text>
+        <Text>Rainy</Text>
+      </View>
+      <View>
+        <View>
+          <View>
+            <Image source={require('./assets/umbrella.png')} style={{ width: 30, height: 30 }} />
+            <Text>RainFall</Text>
+
+          </View>
+          <Text>3cm</Text>
+        </View>
+        <View>
+          <View>
+            <Image source={require('./assets/vector.png')} style={{ width: 30, height: 30 }} />
+            <Text>Wind</Text>
+
+          </View>
+          <Text>19km/h/</Text>
+        </View>
+        <View style={{backgroundColor: "FFFFF"}}>
+          <View>
+            <Image source={require('./assets/water.png')} style={{ width: 30, height: 30 }} />
+            <Text>Humidity</Text>
+
+          </View>
+          <Text>64%</Text>
+        </View>
       </View>
     </LinearGradient>
   );
@@ -22,5 +53,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
 });
